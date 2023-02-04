052b8383c1a5437fffe2ec9b6fafc813e8cd9ced
rab1t <r.leroi@live.nl>
2023-02-04 14:41:47 +0100

    aria-label

--- renderer/PageShell.vue
+++ renderer/PageShell.vue
@@ -28,7 +28,7 @@
         <Link href="/portfolio">Portfolio</Link>
         <Link href="/afspraak">Maak een afspraak</Link>
       </div>
-      <button class="inline lg:hidden" @click="openMenu(true)">
+      <button aria-label="Menu" class="inline lg:hidden" @click="openMenu(true)">
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><rect x="4" y="7.5" width="16" height="1.5"></rect><rect x="4" y="15" width="16" height="1.5"></rect></svg>
       </button>
     </div>
